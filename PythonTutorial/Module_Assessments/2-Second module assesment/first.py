f05475872396d057e594a2e8767596e2b95c55a4
MahmoudTarek3009 <138800622+MahmoudTarek3009@users.noreply.github.com>
2025-01-09 12:40:30 -0800

    Delete PythonTutorial/Module_Assessments directory

--- PythonTutorial/Module_Assessments/2-Second module assesment/first.py
+++
@@ -1,9 +0,0 @@
-def calculate_age(start_year, end_year):
-    return end_year - start_year
-
-start_year = int(input("Enter the year you were born: "))
-end_year = int(input("Enter the current year: "))
-
-age = calculate_age(start_year, end_year)
-
-print(f"The age of the person is: {age} years.")
